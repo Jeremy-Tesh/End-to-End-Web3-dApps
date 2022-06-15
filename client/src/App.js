77403ab7321eba424c11a36520a3f80965a2c06e
jeremy-tesh <jermitesh20@gmail.com>
2022-06-16 01:56:12 +0700

    feat:client setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App bg-black">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
